713feb6859489e4e1180b71c0f1cd8a2152b2975
Igor Shcherbinin <inin097@gmail.com>
2018-09-19 13:34:24 -0400

    Create reverseInteger.java

---
+++ reverseInteger.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args){}
+    public int reverse(int x)
+    {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
